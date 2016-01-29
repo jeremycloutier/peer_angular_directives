57941fd89f8f7d07c56f5b39504d7069096dea72
JeremyCloutier <jeremycloutier@Jeremys-MacBook-Pro.local>
2016-01-29 11:01:03 -0600

    Second commit, adds partial functionality of ng-switch

--- server/public/assets/scripts/client.js
+++ server/public/assets/scripts/client.js
@@ -1,6 +1,9 @@
-var app = angular.module('peerApp', []);
+var app = angular.module('peerApp', ['ngAnimate']);
 
 app.controller('MainController', ['$scope', function($scope){
+    $scope.count = 0;
+    $scope.secondcount = 0;
+
     $scope.foods = ['Soup', 'Salad', 'Sandwich'];
     $scope.selection = $scope.foods[0];
 }]);
